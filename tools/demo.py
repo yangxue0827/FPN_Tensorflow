9507a8d9ed94eec06d7b8f8a5a07d65daa95c98f
yangxue0827 <yangxue0827@126.com>
2017-12-25 15:43:10 +0800

    Update

--- tools/demo.py
+++ tools/demo.py
@@ -408,7 +408,7 @@ def detect_img(file_paths, des_folder, det_th, h_len, w_len, show_res=False):
                 #     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
                 #     img[:, :, 0] = img[:, :, 1] = img[:, :, 2] = img_gray
                 mkdir(des_folder)
-                img_np = draw_box_cv(img,
+                img_np = draw_box_cv(np.array(img, np.float32) - np.array([103.939, 116.779, 123.68]),
                                      boxes=np.array(box_res),
                                      labels=np.array(label_res),
                                      scores=np.array(score_res))
